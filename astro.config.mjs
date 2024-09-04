06177c3df19c804ef1844343aa5e477a45bd1f07
KinhoLeung <kinholeung6119@gmail.com>
2024-09-04 22:49:01 +0800

    🦄 refactor(footer): 优化页脚

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,6 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import {parseDirectiveNode} from "./src/plugins/remark-directive-rehype.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 import {remarkExcerpt} from "./src/plugins/remark-excerpt.js";
-import icon from "astro-icon";
 
 const oklchToHex = (str) => {
   const DEFAULT_HUE = 250
@@ -37,11 +36,6 @@ export default defineConfig({
   trailingSlash: "always",
   integrations: [
     tailwind(),
-    icon({
-      include: {
-        mdi: ["*"], // 包含所有MDI图标
-      },
-    }),
     swup({
       theme: false,
       animationClass: 'transition-swup-',   // see https://swup.js.org/options/#animationselector
