5fa980c557ef6aca67b07292a23c3e0e5c1b8385
KinhoLeung <kinholeung6119@gmail.com>
2024-09-08 14:25:18 +0800

    ✨ feat: 更换icon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,72 +2,72 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '32x32',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '128x128',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '180x180',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '256x256',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '384x384',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'light',
     sizes: '512x512',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '32x32',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '192x192',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '256x256',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '384x384',
   },
   {
-    src: '/favicon/Oathblade-alist.svg',
+    src: '/favicon/Oathblade-alist.png',
     theme: 'dark',
     sizes: '512x512',
   },
